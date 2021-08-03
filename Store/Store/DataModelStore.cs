88e3acc63684f983e9e61afd8df23b8564f055cc
Irina <ten7473@mail.ru>
2021-08-03 22:13:43 +0500

    Создана модель баы данныхб бд подключена

---
+++ Store/Store/DataModelStore.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Goods
+    {
+        public int IdGoods { get; set; }
+        public string NameGoods { get; set; }
+        public string Unit { get; set; }
+        public int IdType { get; set; }
+        
+        public Goods() { }
+        public Goods(int idGoods, string nameGoods, string unit, int idType)
+        {
+            IdGoods = idGoods;
+            NameGoods = nameGoods;
+            Unit = unit;
+            IdType = idType;
+        }
+    }
+
+    public class TypeGoods
+    {
+        public int IdType { get; set; }
+        public string NameType { get; set; }
+        
+        public TypeGoods() { }
+        public TypeGoods(int idType, string nameType)
+        {
+            IdType = idType;
+            NameType = nameType;
+        }
+    }
+
+    public class Suppliers
+    {
+        public int IdSuppliers { get; set; }
+        public string NameSuppliers { get; set; }
+        public string Phone { get; set; }
+
+        public Suppliers() { }
+        public Suppliers(int idSuppliers, string nameSuppliers, string phone)
+        {
+            IdSuppliers = idSuppliers;
+            NameSuppliers = nameSuppliers;
+            Phone = phone;
+        }
+    }
+
+    public class Consignments
+    {
+        public int IdConsignments { get; set; }
+        public string Date { get; set; }
+        public int IdGoods { get; set; }
+        public int IdSuppliers { get; set; }
+        public int Amount { get; set; }
+        public int SupplierPrice { get; set; }
+
+        public Consignments() { }
+        public Consignments(int idConsignments, string date, int idGoods, int idSuppliers, int amount, int supplierPrice)
+        {
+            IdConsignments = idConsignments;
+            Date = date;
+            IdGoods = idGoods;
+            IdSuppliers = idSuppliers;
+            Amount = amount;
+            SupplierPrice = supplierPrice;
+        }
+    }
+}
---
+++ Store/Store/StoreDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace Store
+{
+    public class StoreDB
+    {
+        private string _connectionString;
+        private SqliteConnection _connection;
+        private SqliteCommand _query;
+
+        public StoreDB(string patch)
+        {
+            _connectionString = $"Data Source={patch};Mode=ReadWrite;";
+            try
+            {
+                _connection = new SqliteConnection(_connectionString);
+                _query = new SqliteCommand { Connection = _connection };
+            }
+            catch (Exception)
+            {
+                throw new Exception("Путь к базе данных не найден");
+            }
+        }
+
+        public void Open()
+        {
+            try
+            {
+                _connection.Open();
+                Console.WriteLine("Успешное подключение к базе данных");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new Exception("Ошибка открытия базы данных");
+            }
+            catch (SqliteException)
+            {
+                throw new Exception("Подключаемся к уже открытой базе данных");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Путь к базе данных не найден");
+            }
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+
+
+    }
+}
